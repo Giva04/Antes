9d5343ca5a427340db217b6472b40aca2c2c4fd9
Giva04 <givagomes04@gmail.com>
2023-05-01 15:47:33 -0300

    Coletando informações nos inputs e transformando em variaveis

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // criando função teste
+// function update () {
+//     console.log('ok')
+// }
+// FUNÇÃO QUE FARÁ O CALCULO PEGANDO INFORMAÇÃO DE CADA COMPONTENTE
+// LOGICA DA CALCULADORA
+
 function update () {
-    console.log('ok')
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+    console.log({bill, tipPercent, split})
 }
