bda57e653fdaa8669c3dac2cdf0fc048dcba070f
Giva04 <givagomes04@gmail.com>
2023-05-01 16:35:36 -0300

    Finalizando Logica de calculos com interação no HTML

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // FUNÇÃO QUE CAPTURA AS INFORMAÇÕES DE CADA COMPONTENTE E TRANSFORMA EM VARIAVEL
 
 function update () {
-    let bill = Number(document.getElementById('yourBill').value)
+    let bill = Number(document.getElementById('yourBill') .value )
     let tipPercent = document.getElementById('tipInput').value
     let split = document.getElementById('splitInput').value
     // console.log({bill, tipPercent, split})
@@ -20,7 +20,18 @@ function update () {
     let billTotal =  bill + tipValue
         // dividindo ca conta por pessoas
     let billEach = billTotal / split
-    console.log(billEach)
+    // console.log(billEach)
 
+    // MOSTRANDO OS RESULTADOS NO HTML
+        // porcetagem
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %' 
+        // Gorjeta
+    document.getElementById('tipValue').innerHTML = "R$ " + tipValue
+        // Total da Conta
+    document.getElementById('totalWithTip').innerHTML = "R$ " + billTotal
+        // Split mostra a quantidade de pessoas
+    document.getElementById('splitValue').innerHTML = split
+        //  Total por Pessoa
+    document.getElementById('billEach').innerHTML = "R$ " + billTotal / split
 }
 
